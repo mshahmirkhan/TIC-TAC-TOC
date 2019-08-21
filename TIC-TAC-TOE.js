b0824bc4c6b8f7db7e3a42c8c6123c7277eda495
Muhammad Shahmir Khan <sshhaahhmmiirr@gmail.com>
2019-08-21 13:56:45 +0500

    Incomplete GUI coding

---
+++ TIC-TAC-TOE.js
@@ -0,0 +1,168 @@
+<html>
+
+<head>
+    <title>T-2</title>
+</head>
+
+<style>
+    body {
+        background-color: black;
+    }
+    
+    #title {
+        position: relative;
+        margin-left: 0px;
+        margin-top: 0px;
+        padding-left: 0%;
+        width: 1300px;
+        height: 50px;
+        background-color: rgb(0, 0, 139);
+        align-items: center;
+    }
+    
+    #title_text {
+        color: aliceblue;
+        font-size: 40px;
+        font-weight: 700px;
+        margin-left: 530px;
+    }
+    
+    #vertical_1 {
+        background-color: aliceblue;
+        width: 5px;
+        height: 250px;
+        margin-left: 705px;
+        margin-top: 90px;
+    }
+    
+    #vertical_2 {
+        background-color: aliceblue;
+        width: 5px;
+        height: 250px;
+        margin-left: 600px;
+        margin-top: -250px;
+    }
+    
+    #horizontal_1 {
+        background-color: aliceblue;
+        width: 307px;
+        height: 5px;
+        margin-left: 500px;
+        margin-top: -180px;
+    }
+    
+    #horizontal_2 {
+        background-color: aliceblue;
+        width: 307px;
+        height: 5px;
+        margin-left: 500px;
+        margin-top: 90px;
+    }
+    
+    #a {
+        width: 95px;
+        height: 75px;
+        background-color: aquamarine;
+        margin-left: 500px;
+        margin-top: -180px;
+    }
+    
+    #b {
+        width: 95px;
+        height: 75px;
+        background-color: aquamarine;
+        margin-left: 500px;
+        margin-top: 17px;
+    }
+    
+    #c {
+        width: 95px;
+        height: 75px;
+        background-color: aquamarine;
+        margin-left: 500px;
+        margin-top: 18px;
+    }
+    
+    #d {
+        width: 90px;
+        height: 75px;
+        background-color: aquamarine;
+        margin-left: 610px;
+        margin-top: -260px;
+    }
+    
+    #e {
+        width: 90px;
+        height: 75px;
+        background-color: aquamarine;
+        margin-left: 610px;
+        margin-top: 17px;
+    }
+    
+    #f {
+        width: 90px;
+        height: 75px;
+        background-color: aquamarine;
+        margin-left: 610px;
+        margin-top: 17px;
+    }
+    
+    #g {
+        width: 90px;
+        height: 75px;
+        background-color: aquamarine;
+        margin-left: 715px;
+        margin-top: -260px;
+    }
+    
+    #h {
+        width: 90px;
+        height: 75px;
+        background-color: aquamarine;
+        margin-left: 715px;
+        margin-top: 18px;
+    }
+    
+    #i {
+        width: 90px;
+        height: 75px;
+        background-color: aquamarine;
+        margin-left: 715px;
+        margin-top: 18px;
+    }
+    
+    #footer {
+        margin-top: 15%;
+        color: whitesmoke;
+        font-size: 20px;
+    }
+    
+    i:hover {
+        background-color: blue;
+    }
+</style>
+
+<body>
+    <div id="title">
+        <P id="title_text">TIC-TAC-TOC</P>
+    </div>
+    <div id="vertical_1"></div>
+    <div id="vertical_2"></div>
+    <div id="horizontal_1"></div>
+    <div id="horizontal_2"></div>
+    <div id="a">a</div>
+    <div id="b">b</div>
+    <div id="c">c</div>
+    <div id="d">d</div>
+    <div id="e">e</div>
+    <div id="f">f</div>
+    <div id="g">g</div>
+    <div id="h">h</div>
+    <div id="i">i</div>
+    <div id="footer">
+        <marque scrollamount="3">shahmir</marque>
+    </div>
+
+</body>
+
+</html>
